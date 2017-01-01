de4a0eb37d35402237b843a8a4c8b2f8c4c3655a
Ryan Chang <rmc007@ucsd.edu>
2017-01-01 12:08:18 -0800

    Initial commit

---
+++ app/src/main/java/com/example/ryanchang/constraint/PubMap.java
@@ -0,0 +1,13 @@
+package com.example.ryanchang.constraint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PubMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public__map);
+    }
+}
